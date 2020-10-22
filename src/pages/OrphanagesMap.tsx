595ac3eba565abad8f93469792bde0487a54b721
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2020-10-22 06:33:44 -0300

    I finished the orphanageMap

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Link}   from 'react-router-dom';
+import {Map,TileLayer}   from 'react-leaflet';
 import {FiPlus} from 'react-icons/fi';
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap(){
     return (
@@ -20,7 +22,17 @@ function OrphanagesMap(){
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+                center={[-29.1831808,-51.183616]}
+                zoom={15}
+                style={{width:'100%', height: '100%'}}
+            >
+                
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+                />
+                
+            </Map>   
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
